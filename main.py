17a994aabf17d1e73c024ac3f1f6ea1389ea08c5
abrahamxwhoskins <hoskinsa@lopers.unk.edu>
2024-12-06 20:50:27 +0000

    task tracking program

--- main.py
+++ main.py
@@ -35,14 +35,44 @@ def main():
 # Create a function called display_tasks that takes a list of taks and
 # displays every task in the list.
 
-
-
+def display_task(task)
+  # open the task file in read mode
+  file = open('task.txt','r')
+  # Read first line of file
+  line = file.readline()
+  #Loop if not end of file to continue reading and print 
+  while line != '':
+      print(line.rstrip('\n)'))
+      line = file.readline()
+  # Close the file
+  file.close()
+  
+  #display /print all of the task
+  print(task)
+  continue
 
 
 # Create a function called add_task that takes a list of tasks, prompts
 # the user for another task, and then appends the new tasks to the 
 # end of the list.
 
+def add_tasks(tasks)
+  # append to list of task by adding another list^
+  file = open('task.txt', 'a')
+  # prompt user for description
+  name = input('What is the name of the task? ')
+    # append to index 0
+  # prompt user for description
+  description = input('What is the task description? ')
+    # append to index 1
+  # prompt user for due date
+  date = input('When is the task due by? ')
+    # append to index 2
+  # prompt user for status
+  status = input('Has the task been done yet? ')
+    # append to index 3
+  newTask = [name,description,date,status]
+  tasks.append(newTask)
 
 
 
@@ -51,8 +81,25 @@ def main():
 # displays them to the user, and then lets the user choose one
 # to mark as complete. It will then update the status of the 
 # task in the list and return the updated list.
-
-
+def complete(tasks)
+  # Display the 0 index (title) of each index in tasks
+  for title in tasks:
+    print(title[0])
+  # Prompt user to choose task
+  userTask = input('Which task would you like to choose?')
+    if not userTask.isdigit():
+      userTask = input('Please choose a number option for your task or ''done'':')
+    if userTask.isdigit():
+      index = int(userTask) - 1
+      # Change status of task to 'complete'
+      for i in range(tasks.len())
+        tasks[index][3] = 'complete'
+        #return to main menu
+        break
+    #return to main menu
+    if userTask == 'done':
+      break
+ 
 
 if __name__ == "__main__":
   main()
